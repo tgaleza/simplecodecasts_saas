5c526b32f7d386a2906425fb0777b23c0bf1137b
Tim Galeza <tim.galeza@gmail.com>
2015-10-12 15:41:37 +0000

    Created contact form view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,11 @@
 class ContactsController < ApplicationController
-    def new
-    end
+      def new
+         @contact = Contact.new
+      end    
     
-    def create
-    end
+      def create
+      end
 end
+
 
     
