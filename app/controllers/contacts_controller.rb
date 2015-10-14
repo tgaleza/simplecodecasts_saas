f7dbf530fcd05fe04cbbb561a249e0a8f00cf133
Tim Galeza <tim.galeza@gmail.com>
2015-10-14 13:15:42 +0000

    Created contact form view 2-after error

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,10 @@
 class ContactsController < ApplicationController
       def new
-         @contact = Contact.new
+        @contact = Contact.new
       end    
     
+	
       def create
       end
+
 end
-
-
-    
